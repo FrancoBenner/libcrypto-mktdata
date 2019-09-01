e447660b869f8239c1ba255a892def10a2616e1d
Kyle Downey <kyle.downey@gmail.com>
2019-09-01 11:45:22 -0400

    Fixed all warnings

--- src/print_client_tls.cpp
+++ src/print_client_tls.cpp
@@ -13,16 +13,16 @@ using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 
-void on_message(websocketpp::connection_hdl, client::message_ptr msg) {
+void on_message(const websocketpp::connection_hdl&, const client::message_ptr& msg) {
     std::cout << msg->get_payload() << std::endl;
 }
 
 /// Verify that one of the subject alternative names matches the given hostname
 bool verify_subject_alternative_name(const char * hostname, X509 * cert) {
-    STACK_OF(GENERAL_NAME) * san_names = NULL;
+    STACK_OF(GENERAL_NAME) * san_names = nullptr;
 
-    san_names = (STACK_OF(GENERAL_NAME) *) X509_get_ext_d2i(cert, NID_subject_alt_name, NULL, NULL);
-    if (san_names == NULL) {
+    san_names = (STACK_OF(GENERAL_NAME) *) X509_get_ext_d2i(cert, NID_subject_alt_name, nullptr, nullptr);
+    if (san_names == nullptr) {
         return false;
     }
 
@@ -37,7 +37,7 @@ bool verify_subject_alternative_name(const char * hostname, X509 * cert) {
             continue;
         }
 
-        char * dns_name = (char *) ASN1_STRING_data(current_name->d.dNSName);
+        char * dns_name = (char *) ASN1_STRING_get0_data(current_name->d.dNSName);
 
         // Make sure there isn't an embedded NUL character in the DNS name
         if (ASN1_STRING_length(current_name->d.dNSName) != strlen(dns_name)) {
@@ -61,17 +61,17 @@ bool verify_common_name(const char * hostname, X509 * cert) {
 
     // Extract the CN field
     X509_NAME_ENTRY * common_name_entry = X509_NAME_get_entry(X509_get_subject_name(cert), common_name_loc);
-    if (common_name_entry == NULL) {
+    if (common_name_entry == nullptr) {
         return false;
     }
 
     // Convert the CN field to a C string
     ASN1_STRING * common_name_asn1 = X509_NAME_ENTRY_get_data(common_name_entry);
-    if (common_name_asn1 == NULL) {
+    if (common_name_asn1 == nullptr) {
         return false;
     }
 
-    char * common_name_str = (char *) ASN1_STRING_data(common_name_asn1);
+    char * common_name_str = (char *) ASN1_STRING_get0_data(common_name_asn1);
 
     // Make sure there isn't an embedded NUL character in the CN
     if (ASN1_STRING_length(common_name_asn1) != strlen(common_name_str)) {
@@ -112,13 +112,8 @@ bool verify_certificate(const char * hostname, bool preverified, boost::asio::ss
     if (depth == 0 && preverified) {
         X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
 
-        if (verify_subject_alternative_name(hostname, cert)) {
-            return true;
-        } else if (verify_common_name(hostname, cert)) {
-            return true;
-        } else {
-            return false;
-        }
+        return verify_subject_alternative_name(hostname, cert) ? true
+            : verify_common_name(hostname, cert);
     }
 
     return preverified;
@@ -154,11 +149,11 @@ bool verify_certificate(const char * hostname, bool preverified, boost::asio::ss
  * something equivilent to spoof one of the names on that cert
  * (websocketpp.org, for example).
  */
-context_ptr on_tls_init(const char * hostname, websocketpp::connection_hdl) {
+context_ptr on_tls_init(const char * hostname, const websocketpp::connection_hdl&) {
     context_ptr ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
 
     try {
-        ctx->set_options(boost::asio::ssl::context::default_workarounds |
+        ctx->set_options(boost::asio::ssl::context::default_workarounds | // NOLINT(hicpp-signed-bitwise)
                          boost::asio::ssl::context::no_sslv2 |
                          boost::asio::ssl::context::no_sslv3 |
                          boost::asio::ssl::context::single_dh_use);
